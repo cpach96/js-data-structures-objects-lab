e2216e9d893a53080edf8d7786c3593d716589d4
cpach96 <cpach96@gmail.com>
2021-03-08 12:23:02 -0500

    commit tree 8071fb615c1b4e23fc805a1d75085275344c7017

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+const driver = {};
+
+function updateDriverWithKeyAndValue(driver,key,value){
+    const newDrivers = {...driver};
+    newDrivers[key] = value;
+    return newDrivers;
+};
+
+function destructivelyUpdateDriverWithKeyAndValue(driver,key,value){
+    driver[key] = value;
+    return driver
+};
+
+function deleteFromDriverByKey(driver,key){
+    const newDriver = {...driver};
+    delete newDriver['name']
+    return newDriver;
+}
+
+function destructivelyDeleteFromDriverByKey(driver, key){
+    delete driver['name']
+    return driver
+}
